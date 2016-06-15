ca022bcfca78f4caa4b67cd04d17204614408054
natxo cabre <natxo.cabre@gmail.com>
2016-06-15 20:13:56 +0200

    basic project structure: model-view-update

--- Pimpampum.elm
+++ Pimpampum.elm
@@ -1,4 +1,19 @@
-import Html exposing (Html, span, text)
+import Html exposing (Html, span, text, div, button)
+import Html.App as Html
 import Html.Attributes exposing (class, style)
+import Html.Events exposing (onClick)
+import String exposing (reverse)
 
-main = span [class "welcome-message", style [("color", "blue")]] [text "Hello, World!"]
+main = Html.beginnerProgram { model = "Hello World", view = view, update = update }
+
+type Msg = Reverse
+
+update msg model =
+    case msg of
+        Reverse -> String.reverse model
+
+
+view model =
+    div []
+        [button [onClick Reverse] [text "reverse"]
+        ,text model]
