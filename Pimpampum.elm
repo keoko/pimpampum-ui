74d91e9fc4253b8d7410557ded66bea64101dfdc
natxo cabre <natxo.cabre@gmail.com>
2016-06-17 22:52:05 +0200

    update item

--- Pimpampum.elm
+++ Pimpampum.elm
@@ -7,14 +7,25 @@ import Debug exposing (log)
 main = Html.beginnerProgram { model = initialModel, view = view, update = update }
 
 -- Model
-type Msg = Delete Int | Add | Sku String | Name String | Description String
+type EditMode = AddMode | UpdateMode
+
+type Msg = Edit Int
+         | Delete Int
+         | Update Int
+         | Reset
+         | Add
+         | Sku String
+         | Name String
+         | Description String
 
 type alias Model =
     { items : List Item
     , uid : Int
+    , idField : Int
     , skuField : String
     , nameField : String
     , descriptionField : String
+    , editMode : EditMode
     }
 
 type alias Item =
@@ -26,9 +37,11 @@ type alias Item =
 
 initialModel : Model
 initialModel = { uid = 1
+               , idField = 0
                , skuField = ""
                , nameField = ""
                , descriptionField = ""
+               , editMode = AddMode
                , items = [
                       { id = 1
                       , sku = "sku1"
@@ -51,13 +64,27 @@ newItem uid sku name description =
     }
 
 -- Update
-update msg model = 
+update msg model =
     case msg of
         Delete id ->
             { model | items = List.filter (\i -> i.id /= id) model.items }
+        Edit id ->
+            let
+                items = List.filter (\i -> i.id == id) model.items
+            in
+                case List.head(items) of
+                    Just item -> { model |
+                                   editMode = UpdateMode
+                                 , idField = item.id
+                                 , skuField = item.sku
+                                 , nameField = item.name
+                                 , descriptionField = item.description
+                                 }
+                    Nothing -> model
         Add ->
             { model |
             uid = model.uid + 1
+            , idField = 0
             , skuField = ""
             , nameField = ""
             , descriptionField = ""
@@ -69,12 +96,33 @@ update msg model =
                            model.descriptionField
                          ]
             }
+        Update id ->
+            let
+                updateItem i =
+                    if i.id == id then
+                        { i |
+                          sku = model.skuField
+                        , name = model.nameField
+                        , description = model.descriptionField
+                        }
+                    else
+                        i
+            in
+                { model | items = List.map updateItem model.items }
         Sku sku ->
             { model | skuField = sku }
         Name name ->
             { model | nameField = name }
         Description description ->
             { model | descriptionField = description }
+        Reset ->
+            { model |
+              editMode = AddMode
+            , idField = 0
+            , skuField = ""
+            , nameField = ""
+            , descriptionField = ""
+            }
 
 -- Views
 itemListView items = ul [id "item-list"] (List.map (itemView) items)
@@ -86,18 +134,28 @@ itemView item =
         ,div [] [text "sku:", text item.sku]
         ,div [] [text "description:", text item.description]
         ,button [onClick (Delete item.id)] [text "delete"]
+        ,button [onClick (Edit item.id)] [text "edit"]
         ]
 
 itemFormView model =
     div []
-        [text "sku:"
-        ,input [onInput Sku, value model.skuField] []
-        ,text "name:"
-        ,input [onInput Name, value model.nameField] []
-        ,text "description:"
-        ,input [onInput Description, value model.descriptionField] []
-        ,button [onClick Add] [text "add"]
+        [ text "sku:"
+        , input [onInput Sku, value model.skuField] []
+        , text "name:"
+        , input [onInput Name, value model.nameField] []
+        , text "description:"
+        , input [onInput Description, value model.descriptionField] []
+        , editButton model
+        , resetButton
         ]
+
+editButton model =
+    case model.editMode of
+        AddMode -> button [onClick Add] [text "add"]
+        UpdateMode -> button [onClick (Update model.idField)] [text "update"]
+
+resetButton =
+    button [onClick Reset] [text "reset"]
 
 view model =
     div []
